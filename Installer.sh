6d3bb6ee35893a29edeae20379d6e6dcc4850704
Ali Jafari <43755665+Dr-Cyb3r@users.noreply.github.com>
2018-10-25 21:34:21 +0330

    installer

---
+++ Installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt install python3-pip
+echo "----------------------"
+pip3 install colored
+pip3 install whois
+pip3 install proxybroker
+pip3 install asyncio
+echo "--------------------------------
+Successfully installed
+--------------------------------"
